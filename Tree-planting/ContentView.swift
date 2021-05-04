3fe0be206794b86c4fcb3891fc4503fbfa9591cd
Ahmed <ahmedalharbi85@icloud.com>
2021-05-04 23:24:18 +0300

    Initial Commit

---
+++ Tree-planting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tree-planting
+//
+//  Created by Ahmed Alharbi on 22/09/1442 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tree-planting/Tree_plantingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tree_plantingApp.swift
+//  Tree-planting
+//
+//  Created by Ahmed Alharbi on 22/09/1442 AH.
+//
+
+import SwiftUI
+
+@main
+struct Tree_plantingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
