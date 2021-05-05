3a5f6f74860dd23bcd7e8f179a852a63c8cf9a89
Ahmed <ahmedalharbi85@icloud.com>
2021-05-05 05:18:50 +0300

    added API google map

--- Tree-planting/Tree_plantingApp.swift
+++ Tree-planting/Tree_plantingApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import GoogleMaps
+
+let APIKey = "AIzaSyDzm1UZF7h2YqWkjBN2Bh078b28XyvEnjw"
+
+class AppDelegate: NSObject, UIApplicationDelegate    {
+     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+         GMSServices.provideAPIKey(APIKey)
+         return true
+     }
+ }
 
 @main
 struct Tree_plantingApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
